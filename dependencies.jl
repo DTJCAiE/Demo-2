fd660ab9169bc4183fecff2a1449e390ca830185
Anson Ho <atyho@torontomu.ca>
2023-06-12 17:47:41 +0000

    Update required packages

--- dependencies.jl
+++ dependencies.jl
@@ -1,7 +1,9 @@
 using Pkg
 
 dependencies = [
-    PackageSpec(name="IJulia", version="1.24.2")
+    PackageSpec(name="IJulia", version="1.24.2"),
+    "Compat", "Distributions", "Plots", "CSV", "DataFrames", 
+    "SpecialFunctions", "Statistics", "NLsolve", "Optim", "KernelDensity", "LaTeXStrings"
 ]
 
 Pkg.add(dependencies)
